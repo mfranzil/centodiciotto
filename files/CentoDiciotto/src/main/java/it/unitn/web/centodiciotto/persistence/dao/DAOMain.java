a10d330ce35d1ba7939de3e1f071389de36298af
alessiamarcolini <98marcolini@gmail.com>
2019-08-02 16:27:27 +0200

    Remove silly debugging print statements

--- files/CentoDiciotto/src/main/java/it/unitn/web/centodiciotto/persistence/dao/DAOMain.java
+++ files/CentoDiciotto/src/main/java/it/unitn/web/centodiciotto/persistence/dao/DAOMain.java
@@ -8,13 +8,10 @@ public class DAOMain {
 
     public static void main(String[] args) {
         DAOManager dao = new DAOManager();
-        System.out.println("1");
 
         User user = new User(null, "prova", "prova", "mario", "rossi", 1);
 
-        System.out.println("2");
-
         dao.userDAO.insert(user);
-        System.out.println("3");
+
     }
 }
