f519b8b4185533e655e35389aafc1efdca1ed9b1
simonenascivera <simone.nascivera@gmail.com>
2019-11-08 12:37:36 +0100

    initial doctorServlet and examBooking servlet work

---
+++ src/main/java/it/unitn/web/centodiciotto/servlets/shared/DoctorServlet.java
@@ -0,0 +1,111 @@
+package it.unitn.web.centodiciotto.servlets.shared;
+
+import com.google.gson.Gson;
+import it.unitn.web.centodiciotto.persistence.dao.SpecializedDoctorDAO;
+import it.unitn.web.centodiciotto.persistence.dao.exceptions.DAOFactoryException;
+import it.unitn.web.centodiciotto.persistence.dao.factories.DAOFactory;
+import it.unitn.web.centodiciotto.persistence.entities.Patient;
+import it.unitn.web.centodiciotto.persistence.entities.User;
+import it.unitn.web.centodiciotto.utils.JsonUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(urlPatterns = {"/restricted/patient/doctors"})
+public class DoctorServlet extends HttpServlet {
+    private SpecializedDoctorDAO specializedDoctorDAO;
+
+    @Override
+    public void init() throws ServletException {
+        DAOFactory daoFactory = (DAOFactory) super.getServletContext().getAttribute("daoFactory");
+        if (daoFactory == null) {
+            throw new ServletException("DAOFactory is null.");
+        }
+        try {
+            specializedDoctorDAO = daoFactory.getDAO(SpecializedDoctorDAO.class);
+
+        } catch (DAOFactoryException e) {
+            throw new ServletException("Error in DAO retrieval: ", e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (user instanceof Patient) {
+            doPost(request, response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        String requestType = request.getParameter("requestType");
+
+        switch (requestType) {
+            case "doctorSearch": {
+                if (user instanceof Patient) {
+                    String userInput = request.getParameter("term");
+                    String examID = request.getParameter("examID");
+
+                    List<DoctorSearchResult> results = new ArrayList<>();
+                    /*
+                    if (userInput == null) {
+                        results = ALL_INTERNAL_EXAMS;
+                    } else {
+                        List<ExamSearchResult> tmpResults = new ArrayList<>();
+                        ALL_INTERNAL_EXAMS.stream().filter(exam_SearchResult_
+                                -> (exam_SearchResult_.getText().toLowerCase().contains(userInput.toLowerCase()))).forEach(tmpResults::add);
+                        results = tmpResults;
+                    }
+                    */
+                    Gson gson = new Gson();
+                    response.setContentType("application/json");
+                    response.getWriter().write(gson.toJson(new Results(results.toArray(new ExamSearchResult[0]))));
+                }
+                break;
+            }
+        }
+    }
+
+
+    private static class DoctorSearchResult {
+        private String doctor;
+        private JsonUtils.Action action;
+
+        DoctorSearchResult(String doctor, JsonUtils.Action action) {
+            this.doctor = doctor;
+            this.action = action;
+        }
+    }
+
+    private static class ExamSearchResult implements Serializable {
+        private Integer id;
+        private String text;
+
+        ExamSearchResult(Integer id, String text) {
+            this.id = id;
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+    }
+
+    private static class Results implements Serializable {
+        private ExamSearchResult[] results;
+
+        Results(ExamSearchResult[] results) {
+            this.results = results;
+        }
+    }
+}
